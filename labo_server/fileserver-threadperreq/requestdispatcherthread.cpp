b82e5d47e1b98d19fdc9eabea8f4ff570946d518
Elie Kin <elie.ndjoli@outlook.com>
2018-05-23 15:02:33 +0200

    maj

--- labo_server/fileserver-threadperreq/requestdispatcherthread.cpp
+++ labo_server/fileserver-threadperreq/requestdispatcherthread.cpp
@@ -12,7 +12,10 @@ void RequestDispatcherThread::run(){
 
     while(true){
         SendRequestThread* newThread;
+
+        //Attente qu'il ya ai une valeur dans le buffer
         request = requestsBuffer->get();
+
         newThread = new SendRequestThread(request, responsesBuffer, hasDebugLog);
         connect(newThread, &SendRequestThread::finished, newThread, &QObject::deleteLater);
         newThread->start();
